7def7b7a9551353393cf0d79e104cb1e229220a3
Arne E <odenesser@gmail.com>
2020-01-14 15:09:49 +0100

    did some stuff

--- assignment4.py
+++ assignment4.py
@@ -1,5 +1,12 @@
+import json
 seattle_stationcode = 'GHCND:US1WAKG0038'
-with open('precipitation.json', 'w', encoding='utf8') as file:
+# Loading the .json file into python
+with open('ucaccmet2j_python/precipitation.json', encoding='utf8') as file:
     data = json.load(file)
-    
 
+precipitation = []
+for measurement in data:
+    if (measurement['station']) == seattle_stationcode:
+        print(measurement['value'])
+            
+        # measurement_month = sum(precipitation)
